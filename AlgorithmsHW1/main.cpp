dae271a2248fa42d011a773f57b941dbf073bffc
chrisevancarter <chrisevancarter@gmail.com>
2018-01-20 15:34:50 -0500

    intial commit:

---
+++ AlgorithmsHW1/main.cpp
@@ -0,0 +1,61 @@
+
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// prototypes
+void show( int[], int );
+void shuffle( int[], int );
+void swap( int&, int& );
+void min_search( int[], int, int&, int& );
+void naive_search( int[], int, int&, int&, int& );
+void recursive_search( int[], int, int&, int&, int& );
+void iterative_search( int[], int, int&, int&, int& );
+
+// main driver
+int main( int argc, char *argv[] )
+{
+    // check inputs
+    if ( argc < 3 ) {
+        cerr << "Usage: ./test_me <size> <filename>" << endl;
+        exit(1);
+    }
+
+    // read size
+    int size;
+    size = atoi( argv[1] );
+    cerr << "Size = " << size << endl;
+
+    // open outputfile
+    ofstream outfile;
+    cerr << "Output file = " << argv[2] << endl;
+    outfile.open( argv[2] );
+
+    for (int curr_size = 2; curr_size < size; curr_size++) {
+
+        // create an array of integers 
+        int buffer[curr_size];
+        for (int i = 0; i < curr_size; i++) {
+            buffer[i] = i;
+        }
+
+        // shuffle array
+        shuffle( buffer, curr_size );
+
+        // search for minmax while tracking cost
+        int min, max, cost;
+        cost = 0;
+
+        naive_search( buffer, curr_size, min, max, cost );
+        assert( min == 0 && max == curr_size - 1 );
+        outfile << curr_size << " " << cost << endl;
+    }
+
+    // closing output file 
+    outfile.close();    
+
+}
+
