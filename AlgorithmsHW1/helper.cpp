4374e1fb039502f420991e361d83636a94ee77a9
Jared <jaredjheidt@gmail.com>
2018-01-21 14:14:30 -0500

    tes

--- AlgorithmsHW1/helper.cpp
+++ AlgorithmsHW1/helper.cpp
@@ -3,3 +3,14 @@ void swap(int & a, int & b) {
     b = temp;
     a = b;
 }
+
+// shuffle array
+void shuffle( int buffer[], int size )
+{
+	srand( time(0) );
+	int index;
+	for (int i=size-1; i>0; i--) {
+		index = rand() % size;
+		swap( buffer[index], buffer[i] );
+	}
+}
--- AlgorithmsHW1/main.cpp
+++ AlgorithmsHW1/main.cpp
@@ -1,8 +1,7 @@
-
 #include <stdlib.h>
-#include <assert.h>
 #include <iostream>
 #include <fstream>
+
 
 using namespace std;
 
@@ -10,52 +9,110 @@ using namespace std;
 void show( int[], int );
 void shuffle( int[], int );
 void swap( int&, int& );
-void min_search( int[], int, int&, int& );
-void naive_search( int[], int, int&, int&, int& );
-void recursive_search( int[], int, int&, int&, int& );
-void iterative_search( int[], int, int&, int&, int& );
+void search( int[], int, int& );
+void search( int[], int, int&, int& );
+
+int cost;
 
 // main driver
 int main( int argc, char *argv[] )
 {
-    // check inputs
-    if ( argc < 3 ) {
-        cerr << "Usage: ./test_me <size> <filename>" << endl;
-        exit(1);
-    }
+	// check inputs
+	if ( argc < 3 ) {
+		cerr << "Usage: ./test_me <size> <filename>" << endl;
+		exit(1);
+	}
 
-    // read size
-    int size;
-    size = atoi( argv[1] );
-    cerr << "Size = " << size << endl;
+	// read size
+	int size;
+	size = atoi( argv[1] );
+	cerr << "Size = " << size << endl;
 
-    // open outputfile
-    ofstream outfile;
-    cerr << "Output file = " << argv[2] << endl;
-    outfile.open( argv[2] );
+	// create an array of integers of length size
+	int buffer[size];
+	for (int i=0; i<size; i++) {
+		buffer[i] = i;
+	}
 
-    for (int curr_size = 2; curr_size < size; curr_size++) {
+	// check array
+	show( buffer, size );
 
-        // create an array of integers 
-        int buffer[curr_size];
-        for (int i = 0; i < curr_size; i++) {
-            buffer[i] = i;
-        }
+	// shuffle array
+	shuffle( buffer, size );
 
-        // shuffle array
-        shuffle( buffer, curr_size );
+	// check array
+	show( buffer, size );
 
-        // search for minmax while tracking cost
-        int min, max, cost;
-        cost = 0;
+	// search for minimum
+	int min, max;
+	cost = 0;
+	search( buffer, size, min, max );
+	cerr << "Min = " << min << endl;
+	cerr << "Max = " << max << endl;
+	cerr << "Cost = " << cost << endl;
 
-        naive_search( buffer, curr_size, min, max, cost );
-        assert( min == 0 && max == curr_size - 1 );
-        outfile << curr_size << " " << cost << endl;
-    }
-
-    // closing output file 
-    outfile.close();    
-
+	// open outputfile
+	ofstream outfile;
+	outfile.open( argv[2] );
+	outfile << size << endl;
+	outfile.close();	
 }
+
+
+// minimum finding
+void search( int buffer[], int size, int& min )
+{
+	min = buffer[0];
+	for (int i=1; i<size; i++) {
+		cost++;
+		if ( buffer[i] < min ) {
+			min = buffer[i];
+		}
+	}
+}
+
+// minmax finding
+void search( int buffer[], int size, int& min, int& max )
+{
+	for (int i=1; i<size; i++) {
+		cost++;
+		if ( buffer[i] < buffer[0] ) {
+			swap( buffer[0], buffer[i] );
+		}
+	}
+	min = buffer[0];
+
+	for (int i=2; i<size; i++) {
+		cost++;
+		if ( buffer[i] > buffer[1] ) {
+			swap( buffer[1], buffer[i] );
+		}
+	}
+	max = buffer[1];
+}
+
+
+// show array
+void show( int buffer[], int size )
+{
+	cerr << "[";
+	for (int i=0; i<size; i++) {
+		cerr << buffer[i] << " ";
+	}
+	cerr << "]" << endl;
+}
+
+
+// swap two integers
+void swap( int& a, int& b )
+{
+	int tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+
+
+
 
