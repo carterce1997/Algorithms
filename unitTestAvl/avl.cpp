3ccadd45a05325e801b8f60d65ffbcda115fa7b4
Jared <jaredjheidt@gmail.com>
2018-03-29 20:31:50 -0400

    quick add

--- unitTestAvl/avl.cpp
+++ unitTestAvl/avl.cpp
@@ -6,16 +6,17 @@ using namespace std;
 
 // recurse up tree fixing heights
 template<class T>
-AVL<T> * fixHeightTree(AVL<T> * tree){
+AVL<T> * fixHeightTree(AVL<T> * tree, const int* insertedVal){
 	tree->updateHeight();
 	
 	int rightHeight = tree->getRight() == NULL ? 0 : tree->getRight()->getHeight();
 	int leftHeight = tree->getLeft() == NULL ? 0 : tree->getLeft()->getHeight();
+	int height = leftHeight - rightHeight;
 	
-	if( (rightHeight-leftHeight) > 1){// right-left or right-right case
+	if(  ){// right-left or right-right case
 			
 	}
-	else if( (rightHeight-leftHeight) < -1){// left-heavy
+	else if(  ){// left-left
 	
 	}
 	
@@ -29,21 +30,17 @@ AVL<T> * AVL<T>::insert(const T& x){
 	if(x < this->value){// go left
 		if(this->left == NULL){
 			this->setLeft(new AVL(x, NULL, NULL, this));			
-			return fixHeightTree(this);
+			return fixHeightTree(this, x);// go up the tree fixing nodes
 		}
 		else return (AVL*) this->left->insert(x);
 	}
 	else{
 		if(this->right == NULL){
 			this->setRight(new AVL(x, NULL, NULL, this));
-			return fixHeightTree(this);
+			return fixHeightTree(this, x);
 		}
 		else return (AVL*) this->right->insert(x);
 	}
-	
-	//adjust nodes
-	
-	
 }
 
 // ****************************************************************************
